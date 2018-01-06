ff13bff0554aeb30f020de4f280d9ea7ba4b733d
atalayslayer <35143742+atalayslayer@users.noreply.github.com>
2018-01-06 19:33:16 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cocOS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x = 7;
+            double y = x + 22.3245235325;
+
+            
+
+           
+
+        
+
+            Console.WriteLine(y);
+            Console.ReadLine();
+        }
+    }
+}
